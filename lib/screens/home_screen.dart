cb4451d624f9bc60835c4ff448e67b864936529c
rasheduzzamanrochi <rasheduzzaman2003@gmail.com>
2025-07-19 15:09:30 +0600

    added UI code for fetch data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,31 +10,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final _formKey = GlobalKey<FormState>();
+  late List<Map<String, dynamic>> _employees = [
 
-  final _nameController = TextEditingController();
-  final _ageController = TextEditingController();
-  final _salaryController = TextEditingController();
+  ];
 
-  Future<void> addEmployee() async {
-    final employee = Employee(
-      name: _nameController.text,
-      age: int.parse(_ageController.text),
-      salary: int.parse(_salaryController.text),
-    );
+  Future<void> fetchEmployee() async {
+  }
 
-    try {
-      final response = await SupabaseProvider.client
-          .from('Employee')
-          .insert(employee.toMap());
-
-      if (response == null ||
-          response.error != null ||
-          response.data == null ||
-          response.data.isEmpty) {
-        return;
-      }
-    } catch (e) {}
+  @override
+  void initState() {
+    super.initState();
+    fetchEmployee();
   }
 
   @override
@@ -43,64 +29,25 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(title: Text('Add Employee')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(labelText: 'Name'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a name';
-                  }
-                  return null;
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _employees.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_employees[index]['name']),
+                    subtitle: Text(
+                      'Age: ${_employees[index]['age']}, Salary: ${_employees[index]['salary']}',
+                    ),
+                  );
                 },
               ),
-              TextFormField(
-                controller: _ageController,
-                decoration: InputDecoration(labelText: 'Age'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an age';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _salaryController,
-                decoration: InputDecoration(labelText: 'Salary'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a salary';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    addEmployee();
-                    _nameController.clear();
-                    _ageController.clear();
-                    _salaryController.clear();
-                  }
-                },
-                child: Text('Add Employee'),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _nameController.dispose();
-    _ageController.dispose();
-    _salaryController.dispose();
-    super.dispose();
   }
 }
