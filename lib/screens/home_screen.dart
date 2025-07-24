4a9d9503b48daa63c881a9571708e918f2e6c085
rasheduzzamanrochi <rasheduzzaman2003@gmail.com>
2025-07-25 02:52:29 +0600

    initial commit with adding getX

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,4 @@
-import 'package:employee_api/models/employee_model.dart';
 import 'package:flutter/material.dart';
-import '../services/supabase_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,47 +8,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late List<Map<String, dynamic>> _employees = [];
-
-  Future<void> fetchEmployee() async {
-    final response = await SupabaseProvider.client
-        .from('Employee')
-        .select()
-        .order('id', ascending: true);
-
-    for (var i in response as List) {
-      final data = Employee.fromMap(i);
-
-      _employees.add({
-        'id': data.id,
-        'name': data.name,
-        'age': data.age,
-        'salary': data.salary,
-      });
-    }
-    setState(() {});
-  }
-
-Future<void> deleteEmployee(int id) async {
-    final response = await SupabaseProvider.client
-        .from('Employee')
-        .delete()
-        .eq('id', id);
-
-    if (response.error == null) {
-      _employees.removeWhere((employee) => employee['id'] == id);
-      setState(() {});
-    } else {
-      print('Error deleting employee: ${response.error!.message}');
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchEmployee();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,25 +17,6 @@ Future<void> deleteEmployee(int id) async {
         child: Column(
           children: [
             const SizedBox(height: 8),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _employees.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_employees[index]['name']),
-                    subtitle: Text(
-                      'Age: ${_employees[index]['age']}, Salary: ${_employees[index]['salary']}',
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete_outline),
-                      onPressed: () {
-                        deleteEmployee(_employees[index]['id']);
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
           ],
         ),
       ),
