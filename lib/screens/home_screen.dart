54d98680fbeb1fa28b533daea972d64c7192a067
rasheduzzamanrochi <rasheduzzaman2003@gmail.com>
2025-07-25 03:00:27 +0600

    UI code updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,15 +8,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  signUp() async {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Employee List')),
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: Text('Sign Up'),
+        centerTitle: true,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              keyboardType: TextInputType.visiblePassword,
+            ),
             const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                signUp();
+              },
+              child: Text('Sign Up'),
+            ),
           ],
         ),
       ),
