a3cd4fb3d14144119a1934d758eae91cd96003d4
rasheduzzamanrochi <rasheduzzaman2003@gmail.com>
2025-08-07 01:13:51 +0600

    Initial commit

--- lib/screens/signIn_screen.dart
+++ lib/screens/signIn_screen.dart
@@ -1,6 +1,4 @@
-import 'package:employee_api/services/supabase_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -10,83 +8,13 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-
-  Future<void> signIn() async {
-    try {
-      if (!_formKey.currentState!.validate()) {
-        return;
-      }
-
-      final response = await SupabaseProvider.client.auth.signInWithPassword(
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim(),
-      );
-
-      if (response.user != null) {
-        Get.showSnackbar(
-          GetSnackBar(
-            message: ' ',
-            title: 'Sign In Successful',
-            duration: Duration(seconds: 2),
-            backgroundColor: Colors.green,
-          ),
-        );
-      }
-    } catch (e) {
-      print('Error during sign In: $e');
-      Get.showSnackbar(
-        GetSnackBar(
-          message: 'fail',
-          title: e.toString(),
-          duration: Duration(seconds: 2),
-          backgroundColor: Colors.red,
-        ),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.yellow,
-        title: Text('Sign In'),
+        title: const Text('Employee List'),
         centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(labelText: 'Email'),
-                validator: (value) =>
-                    value?.isEmpty ?? true ? 'Name is required' : null,
-                keyboardType: TextInputType.emailAddress,
-              ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Password'),
-                validator: (value) => (value?.length ?? 0) < 6
-                    ? 'Password must be at least 6 characters'
-                    : null,
-                obscureText: true,
-              ),
-              const SizedBox(height: 8),
-              ElevatedButton(
-                onPressed: () {
-                  signIn();
-                },
-                child: Text('Sign In'),
-              ),
-            ],
-          ),
-        ),
+        backgroundColor: Colors.yellow,
       ),
     );
   }
